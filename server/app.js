c3265e29d2463f3aa24712b0d72a4011d8330274
tabryan90 <tabryan1990@gmail.com>
2024-09-03 20:56:23 -0400

    finished

--- server/app.js
+++ server/app.js
@@ -3,7 +3,12 @@
 // const server = http.createServer((req, res) => {
 
 // STEP 1: Import and initialize server, configure for JSON requests
-// Your code here 
+// Your code here
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 // HTTP Server: Handle route with param (/users/:userId) sending plain-text response
 //     let reqBody = "";
@@ -39,11 +44,22 @@
 // });
 
 // STEP 2: Handle route with param (/users/:userId) sending plain-text response
-// Your code here 
+// Your code here
+
+app.get('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.status(200).send(`User details for userId: ${userId}`)
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('Not Found');
+})
 
 // HTTP Server: Set port and listen for requests
 // const port = 5000;
 // server.listen(port, () => console.log('Server is listening on port', port));
 
 // STEP 1: Set port and listen for requests
-// Your code here 
+// Your code here
+const port = 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
